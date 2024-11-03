473d2af656953c6be51edd4e09eb85a719544c6b
LuisAntonioQuezadaAristega <107262704+LuisAntonioQuezadaAristega@users.noreply.github.com>
2024-11-03 12:50:54 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+class membership:
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        self.discount = 0.00
+    
+
+class features:
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class shoppincart:
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+def main():
+
+    print("In development.")
+
+
+if __name__ == "__main__":
+    main()
